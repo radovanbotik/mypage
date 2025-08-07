a3db8faa5a52fe9ece8dcfab5ace9682966970b1
radovanbotik <radovanbotik@gmail.com>
2025-08-07 18:42:41 +0200

    add logos

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,11 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Flip } from "gsap/Flip";
+import { Javacript } from "@/public/images/techstack/javascript";
+import { TypeScript } from "@/public/images/techstack/typescript";
+import { Tailwind } from "@/public/images/techstack/tailwindcss";
+import { React } from "@/public/images/techstack/react";
+import { Next } from "@/public/images/techstack/nextdotjs";
 
 if (typeof window !== "undefined") {
   gsap.registerPlugin(useGSAP);
@@ -94,6 +99,7 @@ export default function Home() {
             });
           },
         })
+        .to(grid, { gap: 0 }, "<")
         .to(page.current, {
           backgroundColor: "white",
           color: "black",
@@ -112,12 +118,13 @@ export default function Home() {
         .to(iconsInSequence3[0], {
           yPercent: -100,
         })
-        .to(wordsInSentence1[0], {
-          opacity: 1,
-        })
+
         .to(iconsInSequence3[0], {
           x: 0,
           // backgroundColor: "black",
+        })
+        .to(wordsInSentence1[0], {
+          opacity: 1,
         })
         .to(
           wordsInSentence1,
@@ -180,7 +187,7 @@ export default function Home() {
         pinSpacing: true,
         scrub: 1,
         animation: scrollUpAnimation.current,
-        markers: true,
+        // markers: true,
       });
     },
     { scope: page },
@@ -191,10 +198,10 @@ export default function Home() {
       <section className="//pb-4 relative flex h-dvh flex-col" ref={header}>
         <div className="w-full flex-1">
           <div
-            className="flex size-full flex-col items-center justify-center space-y-8"
+            className="flex size-full flex-col items-center justify-center space-y-6"
             ref={h1}
           >
-            <h1 className="font-hanken-grotesk mx-auto text-center text-4xl font-black text-white lg:max-w-6xl xl:text-9xl">
+            <h1 className="font-hanken-grotesk mx-auto text-center text-4xl font-black text-white lg:max-w-6xl xl:text-6xl">
               Building the web, one pixel at a time
             </h1>
             <p className="font-semibold">Witty comment about me</p>
@@ -204,86 +211,98 @@ export default function Home() {
           </div>
         </div>
         <div
-          className="//absolute //bottom-0 flex w-full items-start justify-center gap-0 opacity-100 [&>*]:aspect-square [&>*]:w-full [&>*]:max-w-60"
+          className="//absolute //bottom-0 flex w-full items-start justify-center gap-3 opacity-100 [&>*]:aspect-square [&>*]:w-full [&>*]:max-w-60"
           ref={grid1}
         >
-          <div className="icon1 rounded-xl bg-green-400 object-cover will-change-transform">
-            1
+          <div className="icon1 overflow-hidden rounded-xl object-cover will-change-transform">
+            <Javacript className="size-full" />
           </div>
-          <div className="icon1 rounded-xl bg-amber-400 will-change-transform">
-            2
+          <div className="icon1 overflow-hidden rounded-xl will-change-transform">
+            <TypeScript className="size-full" />
           </div>
-          <div className="icon1 rounded-xl bg-sky-400 will-change-transform">
-            3
+          <div className="icon1 overflow-hidden rounded-xl will-change-transform">
+            <Tailwind className="size-full" />
           </div>
-          <div className="icon1 rounded-xl bg-orange-400 will-change-transform">
-            4
+          <div className="icon1 overflow-hidden rounded-xl will-change-transform">
+            <React className="size-full" />
           </div>
-          <div className="icon1 rounded-xl bg-violet-400 will-change-transform">
-            5
+          <div className="icon1 overflow-hidden rounded-xl will-change-transform">
+            <Next className="size-full" />
           </div>
         </div>
-        <div className="absolute top-1/2 w-full -translate-y-1/2">
+        <div className="font-hanken-grotesk absolute top-1/2 w-full -translate-y-1/2 font-black">
           <div className="relative mx-auto flex h-full w-fit justify-center opacity-100">
             {/* SENTENCE 1 */}
-            <div className="sentence1 absolute -top-full left-0 z-10 flex w-full items-center justify-center gap-1 text-6xl whitespace-nowrap select-none">
+            <div className="sentence1 absolute -top-full left-0 z-10 flex w-full items-center justify-center gap-1 text-5xl whitespace-nowrap select-none">
               <span className="text-content -translate-x-[10%] opacity-0">
-                a word aa aa aaaaa
+                My UI is built
               </span>
-              <div className="icon3 inline-block aspect-square h-[60px] w-[60px] translate-y-full rounded-xl bg-green-300 object-cover opacity-0 will-change-transform"></div>
+              <div className="icon3 inline-block aspect-square h-[60px] w-[60px] translate-y-full opacity-0 will-change-transform">
+                <div className="overflow-hidden rounded-md border-2 border-black">
+                  <Javacript className="size-full" />
+                </div>
+              </div>
               <span className="text-content -translate-x-[10%] opacity-0">
-                b word aaaaa asdasaaaaaa
+                with JavaScript
               </span>
             </div>
             {/* SENTENCE 2 */}
-            <div className="sentence2 absolute top-0 left-0 z-10 flex w-full items-center justify-center gap-1 text-6xl whitespace-nowrap select-none">
+            <div className="sentence2 absolute top-0 left-0 z-10 flex w-full items-center justify-center gap-1 text-5xl whitespace-nowrap select-none">
               <span className="text-content -translate-x-[10%] opacity-0">
-                aeeeaaa
+                TypeScript
               </span>
-              <div className="icon3 inline-block aspect-square h-[60px] w-[60px] rounded-xl bg-amber-300 object-cover opacity-0 will-change-transform"></div>
-              <span className="text-content opacity-0">zoz a</span>
+              <div className="icon3 inline-block aspect-square h-[60px] w-[60px] rounded-xl opacity-0 will-change-transform">
+                <div className="overflow-hidden rounded-md border-2 border-black">
+                  <TypeScript className="size-full" />
+                </div>
+              </div>
+              <span className="text-content opacity-0">
+                handles all my type-checks
+              </span>
             </div>
             {/* SENTENCE 3 */}
-            <div className="sentence3 absolute top-full left-0 z-10 flex w-full items-center justify-center gap-1 text-6xl whitespace-nowrap select-none">
+            <div className="sentence3 absolute top-full left-0 z-10 flex w-full items-center justify-center gap-1 text-5xl whitespace-nowrap select-none">
               <span className="text-content -translate-x-[10%] opacity-0">
-                testttta
+                Designs are beautiful thanks
               </span>
-              <div className="icon3 inline-block aspect-square h-[60px] w-[60px] -translate-y-full rounded-xl bg-sky-300 object-cover opacity-0 will-change-transform"></div>
+              <div className="icon3 inline-block aspect-square h-[60px] w-[60px] -translate-y-full rounded-xl opacity-0 will-change-transform">
+                <div className="overflow-hidden rounded-md border-2 border-black">
+                  <Tailwind className="size-full" />
+                </div>
+              </div>
               <span className="text-content translate-x-[10%] opacity-0">
-                akokokok
+                to Tailwind
               </span>
             </div>
             {/* SENTENCE 4 */}
-            <div className="sentence4 absolute top-[200%] left-0 z-10 flex w-full items-center justify-center gap-1 text-6xl whitespace-nowrap select-none">
+            <div className="sentence4 absolute top-[200%] left-0 z-10 flex w-full items-center justify-center gap-1 text-5xl whitespace-nowrap select-none">
               <span className="text-content -translate-x-[10%] opacity-0">
-                aeeeaaa
+                My
               </span>
-              <div className="icon3 inline-block aspect-square h-[60px] w-[60px] -translate-y-[200%] rounded-xl bg-orange-300 object-cover opacity-1 will-change-transform"></div>
+              <div className="icon3 inline-block aspect-square h-[60px] w-[60px] -translate-y-[200%] rounded-xl opacity-1 will-change-transform">
+                <div className="overflow-hidden rounded-md border-2 border-black">
+                  <React className="size-full" />
+                </div>
+              </div>
               <span className="text-content //translate-x-[10%] opacity-0">
-                and while
+                performance is powered by
               </span>
-              <div className="icon3 inline-block aspect-square h-[60px] w-[60px] -translate-y-[200%] rounded-xl bg-violet-300 object-cover opacity-1 will-change-transform"></div>
+              <div className="icon3 inline-block aspect-square h-[60px] w-[60px] -translate-y-[200%] rounded-xl opacity-1 will-change-transform">
+                <div className="overflow-hidden rounded-md border-2 border-black">
+                  <Next className="size-full" />
+                </div>
+              </div>
               <span className="text-content translate-x-[10%] opacity-0">
-                zoz a
+                React and Next.js.
               </span>
             </div>
 
             {/* ICONS */}
-            <div className="icon2 invsible aspect-square w-[60px] rounded-xl bg-green-300 opacity-0 will-change-transform">
-              12
-            </div>
-            <div className="icon2 invsible aspect-square w-[60px] rounded-xl bg-amber-300 opacity-0 will-change-transform">
-              22
-            </div>
-            <div className="icon2 invsible aspect-square w-[60px] rounded-xl bg-sky-300 opacity-0 will-change-transform">
-              32
-            </div>
-            <div className="icon2 invsible aspect-square w-[60px] rounded-xl bg-orange-300 opacity-0 will-change-transform">
-              42
-            </div>
-            <div className="icon2 invsible aspect-square w-[60px] rounded-xl bg-violet-300 opacity-0 will-change-transform">
-              52
-            </div>
+            <div className="icon2 invsible aspect-square w-[60px] rounded-xl opacity-0 will-change-transform"></div>
+            <div className="icon2 invsible aspect-square w-[60px] rounded-xl opacity-0 will-change-transform"></div>
+            <div className="icon2 invsible aspect-square w-[60px] rounded-xl opacity-0 will-change-transform"></div>
+            <div className="icon2 invsible aspect-square w-[60px] rounded-xl opacity-0 will-change-transform"></div>
+            <div className="icon2 invsible aspect-square w-[60px] rounded-xl opacity-0 will-change-transform"></div>
           </div>
         </div>
       </section>
